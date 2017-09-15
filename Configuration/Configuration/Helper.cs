e60f65b65b7672b08a73e321541083cd57ea0e84
Vitali Donghvani <vitali.donghvani@stratumfive.com>
2017-09-15 22:38:27 +0100

    configuration helper class initial commit

---
+++ Configuration/Configuration/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    public class Helper
+    {
+        public static IConfigurationRoot GetConfigurationRoot(string configFile)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(configFile);
+
+            return builder.Build();
+        }
+
+
+    }
+}
